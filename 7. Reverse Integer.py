6c262321152a0f1fbbf9648a6d61dffeb2a8574a
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-13 22:28:18 -0400

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x < -2**31 or x > 2**31-1:
-            return 0
+       
+        if x < 0:
+            reverse = int("-"+str(-x)[::-1])    
         else:
-            if x < 0:
-                reverse = int("-"+str(-x)[::-1])
-                
-            else:
                 reverse = int(str(x)[::-1])
         if reverse < -2**31 or reverse > 2**31-1:
             return 0
