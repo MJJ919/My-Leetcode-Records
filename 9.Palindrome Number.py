729773fb5a961e8754446d3d08b1492bda625b17
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-13 23:08:13 -0400

    Update 9.Palindrome Number.py
    2 methods.
    The first one converts int to str while the second does not.

--- 9.Palindrome Number.py
+++ 9.Palindrome Number.py
@@ -5,8 +5,29 @@ class Solution(object):
         :rtype: bool
         """
         if x < -2**31 or x > 2**31-1:
-            return false
+            return False
         else:
             palindrome = str(x)[::-1]
         if palindrome == str(x):
             return True
+       
+'''
+Method below does not convert int to str
+'''
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        a = x
+        if x < 0:
+            return False
+        else:
+            reverse = 0
+            while x > 0:
+                reverse = x%10 + reverse*10
+                x = x//10
+        return reverse == a
+
