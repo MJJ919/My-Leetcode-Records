0dbab5054f556cd591eb9e2dbd0e5b19667e1e98
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-15 00:42:15 -0400

    Create 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,43 @@
+'''
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+'''
+
+'''
+Method below uses 2 pointers.
+'''
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = j = 0
+        for num in nums:
+            if num != val:
+                nums[i] = nums[j]
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return i
+
+'''
+Method below doesn't use pointers
+'''
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
