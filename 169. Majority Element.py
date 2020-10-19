77b88dfb12fad1e13f223dc6488fff26d87d58be
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-19 19:18:39 -0400

    Create 169. Majority Element.py

---
+++ 169. Majority Element.py
@@ -0,0 +1,54 @@
+'''
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+'''
+
+'''
+The method below uses hashmap.
+'''
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dict = defaultdict(int)
+        n = len(nums)
+        for i in nums:
+            dict[i] = dict[i] + 1
+            if dict[i] > n/2:
+                return i
+
+
+'''
+The method below uses sorting.
+'''
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        m = len(nums)/2
+        return nums[m]
+        
+        
+'''
+The method below uses Boyer-Moore Voting Algorithm.
+'''
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        candidate = None
+        for i in nums:
+            if count == 0:
+                candidate = i
+            count += (1 if i == candidate else -1)
+        return candidate
