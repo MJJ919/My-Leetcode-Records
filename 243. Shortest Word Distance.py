f05d52025c864dc2d88f51a225caa8a1932b6cba
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-22 20:36:29 -0400

    Create 243. Shortest Word Distance.py

---
+++ 243. Shortest Word Distance.py
@@ -0,0 +1,28 @@
+'''
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+Example:
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Input: word1 = “coding”, word2 = “practice”
+Output: 3
+Input: word1 = "makes", word2 = "coding"
+Output: 1
+'''
+
+class Solution(object):
+    def shortestDistance(self, words, word1, word2):
+        """
+        :type words: List[str]
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        dis = a = b = float('inf')
+        for i, ch in enumerate(words):
+            if ch == word1:
+                a = i
+            if ch ==word2:
+                b = i
+            dis = min(dis, abs(a-b))
+        return dis
