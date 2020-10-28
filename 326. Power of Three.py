91506b81e9da40d1ee6ce664cccca5010aa6480b
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-28 19:44:47 -0400

    Create 326. Power of Three.py

---
+++ 326. Power of Three.py
@@ -0,0 +1,15 @@
+'''
+Given an integer, write a function to determine if it is a power of three.
+'''
+
+'''
+Math problem.
+'''
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 == 3**19 % n
