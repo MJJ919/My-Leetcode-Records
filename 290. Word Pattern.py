99c94c92ba02ef8e83f817481bbd0e8b410288fb
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-27 22:39:24 -0400

    Create 290. Word Pattern.py

---
+++ 290. Word Pattern.py
@@ -0,0 +1,45 @@
+'''
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+'''
+
+'''
+Method below uses 2 dicts.
+'''
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        dict1 ={}
+        dict2 = {}
+        s = s.split()
+        if len(pattern) != len(s):
+            return False
+        for i in range(len(pattern)):
+            c = pattern[i]
+            w = s[i]
+            if c not in dict1:
+                dict1[c] = i
+            if w not in dict2:
+                dict2[w] = i 
+            if dict1[c] != dict2[w]:
+                return False
+        return True
+
+'''
+Method below uses set.
+'''
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        p = pattern
+        s = s.split()
+        return len(set(zip(s, p))) == len(set(s)) == len(set(p)) and len(s) == len(p)
