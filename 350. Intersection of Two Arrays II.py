bbdb46ab3dec9189ba84517b4db712e379ad503f
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-29 20:27:12 -0400

    Create 350. Intersection of Two Arrays II.py

---
+++ 350. Intersection of Two Arrays II.py
@@ -0,0 +1,66 @@
+'''
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+'''
+
+'''
+Method below uses 2 pointers.
+Time:O(nlogn+mlogm)
+Space:O(1)
+'''
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        i = j = 0 
+        a=[]
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                i = i+1
+            elif nums1[i]>nums2[j]:
+                j = j+1
+            else:
+                a.append(nums1[i])
+                i,j = i+1, j+1
+        return a 
+
+
+'''
+Method below uses hashmap.
+Time: O(n+m)
+Space: O(min(n,m))
+'''
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        a = {}
+        res = []
+        for i in nums1:
+            if not a.has_key(i):
+                a[i] = 1
+            else:
+                a[i] += 1
+        for i in nums2:
+            if a.has_key(i) and a[i]!=0:
+                res.append(i)
+                a[i] -= 1
+        return res
+        
+'''
+Medthod below uses hashtable and Collections.Counter function
+'''
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        a = collections.Counter(nums1)
+        res = []
+        for num in nums2:
+            if num in a and a[num] >0:
+                res.append(num)    
+                a[num] -= 1
+        return res
