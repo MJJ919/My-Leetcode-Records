751c65edb14b0fd2db12fba2d2ea969b2bac02dc
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-10-31 16:39:53 -0400

    Update 1.Two_Sum.py

--- 1.Two_Sum.py
+++ 1.Two_Sum.py
@@ -1,3 +1,15 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+
+'''
+Method below uses hashtable.
+Time:O(n)
+Space:O(n)
+'''
+
 class Solution(object):
     def twoSum(self, nums, target):
         
@@ -12,3 +24,15 @@ class Solution(object):
                 compliments[num] = i
             else:
                 return [compliments[target-num], i]
+
+'''
+Method below uses brute force.
+Time:O(n**2)
+Space:O(1)
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0,len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i, j]
