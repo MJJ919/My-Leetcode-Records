cd35589654d6d31e678851366e58a3876612461c
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-11-04 20:13:14 -0500

    Create 36. Valid Sudoku.py

---
+++ 36. Valid Sudoku.py
@@ -0,0 +1,41 @@
+'''
+https://leetcode.com/problems/valid-sudoku/
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+'''
+
+'''
+Method below uses rows, columns and boxs to store numbers in each row, column and box.
+Time:O(1)  Because the size of sudoku is fixed.
+Space:O(1)
+'''
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        rows=[{}for i in range(9)]
+        columns=[{}for i in range(9)]
+        box=[{}for i in range(9)]
+        
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num != '.':
+                    num = int(num)
+                    box_index = (i//3)*3+j//3
+                    rows[i][num]=rows[i].get(num,0)+1
+                    columns[j][num]=columns[j].get(num,0)+1
+                    box[box_index][num]=box[box_index].get(num,0)+1
+                    if rows[i][num]>1 or columns[j][num]>1 or box[box_index][num]>1:
+                        return False
+        return True
