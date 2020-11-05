800e56380778dc8bb400d801b478848a6311c88e
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-11-04 19:43:45 -0500

    Update 35. Search Insert Position.py

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -1,4 +1,32 @@
 '''
+https://leetcode.com/problems/search-insert-position/
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+'''
+
+'''
+Method below uses binary search.
+Time:O(logn)
+Space:O(1)
+'''
+class Solution:
+    def searchInsert(self, nums, target):
+        if target<nums[0]:
+            return 0
+        if target>nums[-1]:
+            return len(nums)
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if target == nums[mid]:
+                return mid
+            elif target>nums[mid]:
+                left = mid+1
+            else:
+                right = mid-1
+        return left
+
+'''
 My method is a little bit awkward.
 '''
 class Solution(object):
