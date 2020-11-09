a37b28a2b4c6e04f4e227560e7c77820abe29d96
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-11-09 00:45:43 -0500

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -1,11 +1,14 @@
 '''
+https://leetcode.com/problems/plus-one
 Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
-
 The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
-
 You may assume the integer does not contain any leading zero, except the number 0 itself.
 '''
 
+'''
+Time:O(n)
+Space:O(1)
+'''
 class Solution(object):
     def plusOne(self, digits):
         """
@@ -28,3 +31,17 @@ class Solution(object):
                     return digits
         # If all the numbers are 9
             return [1]+digits
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        i = ''.join(str(i) for i in digits)
+        num = int(i)+1
+        res = [int(x) for x in str(num)]
+        if len(res)<len(digits):
+            for i in range(0,len(digits)-len(res)):
+                res.insert(0,0)
+        return res
