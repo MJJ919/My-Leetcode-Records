06534f93eca622db812a551a74b6e40c2a94e06f
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-11-09 22:06:42 -0500

    Update 69.sqrt().py

--- 69.sqrt().py
+++ 69.sqrt().py
@@ -1,12 +1,28 @@
 '''
+https://leetcode.com/problems/sqrtx/
 Implement int sqrt(int x).
-
 Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
-
 Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
-
 '''
-
+'''
+Method below uses 2 pointers.
+Time:O(logN)
+Space:O(1)
+'''
+class Solution(object):
+    def mySqrt(self, x):
+        if x<2:
+            return x
+        i,j = 0,x//2
+        while i<=j:
+            mid = (i+j)//2
+            if mid*mid>x:
+                j=mid-1
+            elif mid*mid<x:
+                i = mid+1
+            else:
+                return mid
+        return j
 '''
 This way is pretty dumb.
 '''
