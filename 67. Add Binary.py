073808d9d3dc59234320ec8e0aca4da9016d7f04
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-11-09 21:48:23 -0500

    Create 67. Add Binary.py

---
+++ 67. Add Binary.py
@@ -0,0 +1,27 @@
+'''
+https://leetcode.com/problems/add-binary/
+Given two binary strings, return their sum (also a binary string).
+The input strings are both non-empty and contains only characters 1 or 0.
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+'''
+'''
+Time:O(m+n)
+Space:O(1)
+'''
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        a1=int(a,2)
+        b1=int(b,2)
+        out = bin(a1+b1)
+        return out[2:]
