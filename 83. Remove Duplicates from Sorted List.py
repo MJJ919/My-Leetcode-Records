c643e6789cd5639c93336ffc34adc9df57f83b97
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-11-14 00:30:00 -0500

    Update 83. Remove Duplicates from Sorted List.py

--- 83. Remove Duplicates from Sorted List.py
+++ 83. Remove Duplicates from Sorted List.py
@@ -1,31 +1,29 @@
 '''
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 Given a sorted linked list, delete all duplicates such that each element appear only once.
-'''
+Example 1:
+Input: 1->1->2
+Output: 1->2
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+Example 2:
+Input: 1->1->2->3->3
+Output: 1->2->3
+'''
+'''
+Time:O(n)
+Space:O(1)
+'''
 class Solution(object):
     def deleteDuplicates(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        if head == None:
-            return head
-
-        current = head.next
-        prev = head
-        
-        while current != None:
-            if current.val == prev.val:
-                prev.next= current.next
-                current = current.next
+        cur=head
+        while cur != None and cur.next!=None:
+            if cur.val == cur.next.val:
+                cur.next=cur.next.next
             else:
-                current = current.next
-                prev = prev.next
+                cur = cur.next
         return head
-                
                 
