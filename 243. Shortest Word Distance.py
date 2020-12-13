8f3fccb42fac6bd0f596e52a54976d9d625a9154
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-13 11:16:38 -0500

    Update 243. Shortest Word Distance.py

--- 243. Shortest Word Distance.py
+++ 243. Shortest Word Distance.py
@@ -1,15 +1,18 @@
 '''
+https://leetcode.com/problems/shortest-word-distance/
 Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
 
 Example:
 Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
-
 Input: word1 = “coding”, word2 = “practice”
 Output: 3
 Input: word1 = "makes", word2 = "coding"
 Output: 1
 '''
-
+'''
+Time:O(n)
+Space:O(1)
+'''
 class Solution(object):
     def shortestDistance(self, words, word1, word2):
         """
