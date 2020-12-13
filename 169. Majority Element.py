313ba75e320c9e872f9f7f3d15615a6a3ba710cd
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-12 22:42:02 -0500

    Update 169. Majority Element.py

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,13 +1,14 @@
 '''
+https://leetcode.com/problems/majority-element/
 Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-
 You may assume that the array is non-empty and the majority element always exist in the array.
 '''
 
 '''
 The method below uses hashmap.
+Time:O(n)
+Space:O(n)
 '''
-
 class Solution(object):
     def majorityElement(self, nums):
         """
@@ -21,9 +22,10 @@ class Solution(object):
             if dict[i] > n/2:
                 return i
 
-
 '''
 The method below uses sorting.
+Time:O(nlgn)
+Space:O(1)
 '''
 class Solution(object):
     def majorityElement(self, nums):
@@ -34,10 +36,11 @@ class Solution(object):
         nums.sort()
         m = len(nums)/2
         return nums[m]
-        
-        
+                
 '''
 The method below uses Boyer-Moore Voting Algorithm.
+Time:O(n)
+Space:O(1)
 '''
 class Solution(object):
     def majorityElement(self, nums):
