94c7eb9d75788f03dfb147283c3562b7fabd95a2
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-16 00:15:37 -0500

    Update 75. Sort Colors.py

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -25,20 +25,13 @@ Output: [0]
 Time:O(N)
 Space:O(1)
 '''
-class Solution(object):
-    def sortColors(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        p0, cur, p2= 0, 0, len(nums)-1
-        while cur<=p2:
-            if nums[cur]==0:
-                nums[cur], nums[p0] = nums[p0], nums[cur]
-                p0 = p0+1
-                cur = cur + 1
-            elif nums[cur]==1:
-                cur = cur+1
+        cur, i, j = 0, 0, len(nums)-1
+        while cur <= j:
+            if nums[cur] == 0:
+                nums[i], nums[cur] = nums[cur], nums[i]
+                i, cur = i+1, cur+1
+            elif nums[cur] == 1:
+                cur += 1                
             else:
-                nums[cur], nums[p2] = nums[p2], nums[cur]
-                p2 = p2-1
+                nums[cur], nums[j] = nums[j], nums[cur]
+                j -= 1
