1b88c1ffa10bb03b2afd96972044e71cf7f258d1
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-19 10:05:23 -0500

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -2,19 +2,18 @@
 Given two strings s and t , write a function to determine if t is an anagram of s.
 
 Example 1:
-
 Input: s = "anagram", t = "nagaram"
 Output: true
-Example 2:
 
+Example 2:
 Input: s = "rat", t = "car"
 Output: false
 '''
 
 '''
-Method below uses dictionary.
+Time:O(n)
+Space:O(1)
 '''
-
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -22,28 +21,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        dict1 = {}
+        d1, d2 = defaultdict(int), defaultdict(int)
         for i in s:
-            if i in dict1:
-                dict1[i] = dict1[i]+1
-            else:
-                dict1[i]=0
-        dict2 = {}
-        for i in t:
-            if i in dict2:
-                dict2[i] = dict2[i]+1
-            else:
-                dict2[i]=0
-        return dict1 == dict2
+            d1[i] += 1
+        for j in t:
+            d2[j] += 1
+        return d1 == d2
         
 '''
-Meethod below uses sorted function.
+Time:O(nlgn)
+Space:O(1)
 '''
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
         return sorted(s) == sorted(t)
