aa9f97420eb786ff313ac211bae7bb882defe19e
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-22 10:43:32 -0500

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,14 +1,11 @@
 '''
+https://leetcode.com/problems/longest-palindromic-substring/
 Given a string s, return the longest palindromic substring in s.
 
 Example 1:
 Input: s = "babad"
 Output: "bab"
 Note: "aba" is also a valid answer.
-
-Example 2:
-Input: s = "cbbd"
-Output: "bb"
 
 Example 3:
 Input: s = "a"
@@ -20,9 +17,9 @@ Output: "a"
 '''
 
 '''
+https://leetcode.com/problems/longest-palindromic-substring/discuss/2954/Python-easy-to-understand-solution-with-comments-(from-middle-to-two-ends)
 Time:O(n**2)
 Space:O(1)
-https://leetcode.com/problems/longest-palindromic-substring/discuss/2954/Python-easy-to-understand-solution-with-comments-(from-middle-to-two-ends)
 '''
 class Solution(object):
     def longestPalindrome(self, s):
@@ -30,18 +27,12 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        m = ''
-        for i in xrange(len(s)):
-            a = self.helper(s,i,i)
-            if len(m)<len(a):
-                m = a           # Or: m=max(m,a,key=len)
-            a = self.helper(s,i,i+1)
-            if len(m)<len(a):
-                m = a
-        return m
-            
-            
+        res = ''
+        for i in range(len(s)):
+            res = max(self.helper(s, i, i), self.helper(s, i, i+1), res, key = len)
+        return res
+    
     def helper(self, s, l, r):
-        while l>=0 and r<len(s) and s[l]==s[r]:
-            l,r=l-1,r+1
+        while l>=0 and r<len(s) and s[l] == s[r]:
+                l, r = l-1, r+1
         return s[l+1:r]
