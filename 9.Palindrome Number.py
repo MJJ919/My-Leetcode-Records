febfd25434e0fc323d55e6299d3735e1483cc9b5
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-21 22:31:52 -0500

    Update 9.Palindrome Number.py

--- 9.Palindrome Number.py
+++ 9.Palindrome Number.py
@@ -1,9 +1,12 @@
 '''
+https://leetcode.com/problems/palindrome-number/
 Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
-
 Follow up: Could you solve it without converting the integer to a string?
 '''
-
+'''
+Time:O(n)
+Space:O(1)
+'''
 class Solution(object):
     def isPalindrome(self, x):
         """
@@ -16,6 +19,20 @@ class Solution(object):
             palindrome = str(x)[::-1]
         if palindrome == str(x):
             return True
+
+'''
+Time:O(n)
+Space:O(1)
+'''
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        s = str(x)
+        for i,ch in enumerate(s):
+            if ch != s[len(s)-i-1]:
+                return False
+        return True
        
 '''
 Method below does not convert int to str
@@ -25,10 +42,6 @@ Space:O(1)
 
 class Solution(object):
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
         a = x
         if x < 0:
             return False
@@ -38,4 +51,3 @@ class Solution(object):
                 reverse = x%10 + reverse*10
                 x = x//10
         return reverse == a
-
