4544f71dbd0a69c42b78a71ad9dca2fec2ad19d2
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-23 00:24:26 -0500

    Update 367. Valid Perfect Square.py

--- 367. Valid Perfect Square.py
+++ 367. Valid Perfect Square.py
@@ -1,6 +1,6 @@
 '''
+https://leetcode.com/problems/valid-perfect-square/
 Given a positive integer num, write a function which returns True if num is a perfect square else False.
-
 Follow up: Do not use any built-in library function such as sqrt.
 '''
 
@@ -15,15 +15,15 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        if num<2:
-            return True
-        a, b = 2, num//2
-        while a<=b:
-            mid = a + (b-a)//2
-            if mid*mid == num:
+        if num < 2: return True
+        i, j = 1, num/2
+        while i<=j:
+            mid = (i+j)/2
+            sqr = mid*mid
+            if sqr == num:
                 return True
-            if mid*mid < num:
-                a = mid+1
+            elif sqr > num:
+                j = mid-1
             else:
-                b = mid-1
+                i = mid+1
         return False
