3dfcdb291be1f3bc19e89a7df775440126608d71
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-24 09:00:28 -0500

    Update 257. Binary Tree Paths.py

--- 257. Binary Tree Paths.py
+++ 257. Binary Tree Paths.py
@@ -11,37 +11,28 @@ Input:
 2     3
  \
   5
-
 Output: ["1->2->5", "1->3"]
-Explanation: All root-to-leaf paths are: 1->2->5, 1->3
 '''
-
 '''
 Time:O(N)
 Space:O(N)
 '''
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
-    def binaryTreePaths(self,root):
+    def binaryTreePaths(self, root):
         """
         :type root: TreeNode
         :rtype: List[str]
         """
-        def construct(root, path):
-            if root:
-                path += str(root.val)
-                if not root.left and not root.right:
-                    paths.append(path)
-                else:
-                    path += '->'
-                    construct(root.right, path)
-                    construct(root.left, path)
+        def helper(node, path):
+            if not node:
+                return
+            path += str(node.val)
+            if not node.left and not node.right:
+                res.append(path)
+            path += '->'
+            helper(node.left, path)
+            helper(node.right, path) 
         
-        paths=[]
-        construct(root, '')
-        return paths
+        res = []
+        helper(root, '')
+        return res
