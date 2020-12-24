0cdf9fd3bc19c9ce5d4f0e8df6749af7b226cac5
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-23 20:42:54 -0500

    Create 144. Binary Tree Preorder Traversal.py

---
+++ 144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,32 @@
+'''
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+'''
+'''
+Time:
+Space:
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.rec(root, res)
+        return res
+    
+    def rec(self, node, res):
+        if node:
+            res.append(node.val)
+            self.rec(node.left, res)
+            self.rec(node.right, res)
