671df9ac08aa823695d8641f1d738d4f67ba02ab
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-25 11:33:28 -0500

    Update 110. Balanced Binary Tree.py

--- 110. Balanced Binary Tree.py
+++ 110. Balanced Binary Tree.py
@@ -1,27 +1,25 @@
 '''
+https://leetcode.com/problems/balanced-binary-tree/
 Given a binary tree, determine if it is height-balanced.
-
 For this problem, a height-balanced binary tree is defined as:
-
 a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
 '''
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def height(self, root):
-        if not root:
-            return -1
-        return 1 + max(self.height(root.left), self.height(root.right))
-    
+'''
+Time:O(nlgn)
+Space:O(n)
+'''
+class Solution(object):
     def isBalanced(self, root):
-        if not root:
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:    
             return True
-        return abs(self.height(root.left) - self.height(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
- 
- 
- 
+        return abs(self.helper(root.left)-self.helper(root.right)) < 2 and self.isBalanced(root.right) and self.isBalanced(root.left)
+    
+    def helper(self, node):
+            if not node:
+                return -1
+            else:   
+                return 1+max(self.helper(node.left),self.helper(node.right))
