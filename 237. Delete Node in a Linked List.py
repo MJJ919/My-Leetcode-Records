74ccee9f71768be0daae8d51b14950089340fe7b
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-29 11:15:53 -0500

    Update 237. Delete Node in a Linked List.py

--- 237. Delete Node in a Linked List.py
+++ 237. Delete Node in a Linked List.py
@@ -1,20 +1,15 @@
 '''
-Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+https://leetcode.com/problems/delete-node-in-a-linked-list/
+Write a function to delete a node in a singly-linked list. 
+You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
 
 It is guaranteed that the node to be deleted is not a tail node in the list.
 
 '''
-
 '''
-It's a trick.
+Time:O(1)
+Space:O(1)
 '''
-
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
     def deleteNode(self, node):
         """
