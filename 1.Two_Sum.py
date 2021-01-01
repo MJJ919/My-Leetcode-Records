e07fde1bf87205ae6b6b5f461d8e52e57cc4f08b
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-31 22:01:18 -0500

    Update 1.Two_Sum.py

--- 1.Two_Sum.py
+++ 1.Two_Sum.py
@@ -1,30 +1,23 @@
 '''
+https://leetcode.com/problems/two-sum/
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 '''
-
 '''
 Method below uses hashtable.
 Time:O(n)
 Space:O(n)
 '''
-
-class Solution(object):
-    def twoSum(self, nums, target):
-        
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        compliments = {}
-        for i, num in enumerate(nums):
-            if target-num not in compliments:
-                compliments[num] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = defaultdict(int)
+        for i, ch in enumerate(nums):
+            if target-ch not in d:
+                d[ch] = i
             else:
-                return [compliments[target-num], i]
-
+                return [i, d[target-ch]]
+            
 '''
 Method below uses brute force.
 Time:O(n**2)
