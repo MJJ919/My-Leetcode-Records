e38223d085f1acc6cf08dddc3525335ea09851a2
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2020-12-31 22:14:00 -0500

    Update 167. Two Sum II - Input array is sorted.py

--- 167. Two Sum II - Input array is sorted.py
+++ 167. Two Sum II - Input array is sorted.py
@@ -1,14 +1,16 @@
 '''
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
-
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
 
 Note:
-
 Your returned answers (both index1 and index2) are not zero-based.
 You may assume that each input would have exactly one solution and you may not use the same element twice.
 '''
-
+'''
+Time:O(n)
+Space:O(n)
+'''
 class Solution(object):
     def twoSum(self, numbers, target):
         """
@@ -16,23 +18,19 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        a = {}
-        for i,ch in enumerate(numbers):
-            if ch in a:
-                return(a[ch], i+1)
-            a[target-ch]=i+1
+        d = defaultdict(int)
+        for i, ch in enumerate(numbers):
+            if target-ch not in d:
+                d[ch] = i+1
+            else:
+                return [d[target-ch], i+1]
 
 '''
-Method below uses two pointers.
+Time:O(n)
+Space:O(1)
 '''
-
 class Solution(object):
     def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         i = 0
         j = len(numbers)-1
         while i<j:
@@ -43,4 +41,3 @@ class Solution(object):
                 j = j-1
             if sum < target:
                 i = i+1
-        
