66f774c3ed988ecc7932dbcef01d66df53497fb4
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-01 12:10:07 -0500

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,7 +1,6 @@
 '''
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
 Find all unique triplets in the array which gives the sum of zero.
-
 Notice that the solution set must not contain duplicate triplets.
 
 Example 1:
@@ -16,34 +15,34 @@ Example 3:
 Input: nums = [0]
 Output: []
 '''
-
 '''
 Method below uses twosum.
 Time:O(n**2)
 Space:O(logn) to log(n)
 '''
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         res = []
         nums.sort()
         for i in range(len(nums)):
-            if nums[i] > 0:
+            if nums[i]>0:
                 break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
+            if i==0 or nums[i] != nums[i-1]:
+                self.sum(nums, i, res)
         return res
-
-    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]):
-        lo, hi = i + 1, len(nums) - 1
-        while (lo < hi):
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
-                lo += 1
-            elif sum > 0:
-                hi -= 1
-            else:
-                res.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
-                    lo += 1
+        
+    def sum(self, nums, i, res):
+        seen = set()
+        j = i+1            
+        while j<len(nums):
+            comp = -nums[i]-nums[j]
+            if comp in seen:
+                res.append([comp, nums[i], nums[j]])
+                while j+1 < len(nums) and nums[j] == nums[j+1]:
+                    j += 1
+            seen.add(nums[j])
+            j += 1
