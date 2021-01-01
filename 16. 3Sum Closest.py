05acf6966300f283a79cd7c48ff5f6a3195526b6
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-01 12:09:11 -0500

    Update 16. 3Sum Closest.py

--- 16. 3Sum Closest.py
+++ 16. 3Sum Closest.py
@@ -2,9 +2,7 @@
 Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. 
 Return the sum of the three integers. You may assume that each input would have exactly one solution.
 '''
-
 '''
-Method below uses 2 pointers.
 Time:O(n**2)
 Space:O(1)
 '''
@@ -15,18 +13,18 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        diff = float('inf')
-        nums = sorted(nums)
-        for i, ch in enumerate(nums):
-            a,b = i+1, len(nums)-1
-            while a<b:
-                sum = ch+nums[a]+nums[b]
-                if abs(sum-target)<abs(diff):
-                    diff = target - sum
-                if sum<target:
-                    a = a+1
+        m = float('inf')
+        nums.sort()
+        for n in range(len(nums)):
+            i, j = n+1, len(nums)-1
+            while i<j:
+                s = nums[n]+nums[i]+nums[j]
+                if abs(target-s)<abs(m):
+                    m = target - s
+                if s>target:
+                    j -= 1
                 else:
-                    b = b-1
-                if diff==0:
-                    break
-        return target - diff
+                    i += 1
+            if m == 0:
+                break
+        return target - m
