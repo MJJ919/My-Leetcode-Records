c088290d3b6e219a831989dc61c3b02c3751b3ec
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-01 10:02:08 -0500

    Update 50. Pow(x, n).py

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -2,10 +2,6 @@
 https://leetcode.com/problems/powx-n/
 Implement pow(x, n), which calculates x raised to the power n (i.e. xn).
 
-Example 1:
-Input: x = 2.00000, n = 10
-Output: 1024.00000
-
 Example 2:
 Input: x = 2.10000, n = 3
 Output: 9.26100
@@ -20,15 +16,25 @@ Method below uses python buildin function POW().
 '''
 class Solution(object):
     def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
         return pow(x,n)
 
 '''
-Method below uses iterative.
+Recursion
+Time:O(lgn)
+Space:O(lgn)
+'''
+class Solution(object):
+    def myPow(self, x, n):
+        if n == 0:
+            return 1
+        if n<0:
+            return 1/self.myPow(x,-n)
+        if n%2:
+            return x*self.myPow(x, n-1)
+        return self.myPow(x*x, n/2)
+ 
+'''
+Iterative
 Time:O(logN)
 Space:O(1)
 '''
