457a0d3746ccb11678bf6879123d56890dc5f005
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-02 10:36:18 -0500

    Create 155. Min Stack.py

---
+++ 155. Min Stack.py
@@ -0,0 +1,49 @@
+'''
+https://leetcode.com/problems/min-stack/
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+'''
+'''
+Time:O(1) for all operations
+Space:O(n)
+'''
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        if not self.stack:
+            self.stack.append([x,x])
+            return 
+        curmin = self.stack[-1][1]
+        self.stack.append([x,min(curmin,x)])
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
