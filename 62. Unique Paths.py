fd28bc7b693ac586d2988b51c4645d30602b3bd2
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-05 16:32:26 -0500

    Update 62. Unique Paths.py

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -5,22 +5,15 @@ The robot can only move either down or right at any point in time.
 The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
 How many possible unique paths are there?
 '''
-
 '''
 This is a math method.
 Time:O(M*N)
 Space:O(M*N)
 '''
-class Solution(object):
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-        d = [[1]*n for i in range(m)] # set all the square to be 1.
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        d = [[1]*n for i in range(m)]
         for i in range(1,m):
             for j in range(1,n):
                 d[i][j] = d[i-1][j]+d[i][j-1]
-        
-        return d[m-1][n-1]
+        return d[-1][-1]
