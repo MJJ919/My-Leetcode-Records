5b8594f56b3c056132d7dfd579a4c15014539a79
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-06 20:19:03 -0500

    Create 91. Decode Ways.py

---
+++ 91. Decode Ways.py
@@ -0,0 +1,33 @@
+'''
+https://leetcode.com/problems/decode-ways/
+A message containing letters from A-Z can be encoded into numbers using the following mapping:
+'A' -> "1"
+'B' -> "2"
+...
+'Z' -> "26"
+To decode an encoded message, all the digits must be mapped back into letters using the reverse of the mapping above (there may be multiple ways). 
+Given a non-empty string num containing only digits, return the number of ways to decode it.
+The answer is guaranteed to fit in a 32-bit integer.
+
+Example 1:
+Input: s = "12"
+Output: 2
+Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+'''
+'''
+Time:O(n)
+Space:O(n)
+'''
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s:   return 0
+        dp = [0 for _ in range(len(s)+1)]
+        dp[0] = 1
+        dp[1] = 0 if s[0]=='0' else 1
+        for i in range(2,len(dp)):
+            if s[i-1]!='0':
+                dp[i] += dp[i-1] 
+            two = int(s[i-2:i])
+            if two>=10 and two<=26:
+                dp[i] += dp[i-2]
+        return dp[-1]
