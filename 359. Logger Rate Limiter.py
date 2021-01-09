c1373e7b66628bc640fd0bf364641d61db6c7471
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-09 07:05:12 -0500

    Update 359. Logger Rate Limiter.py

--- 359. Logger Rate Limiter.py
+++ 359. Logger Rate Limiter.py
@@ -2,11 +2,8 @@
 Design a logger system that receive stream of messages along with its timestamps, each message should be printed if and only if it is not printed in the last 10 seconds.
 
 Given a message and a timestamp (in seconds granularity), return true if the message should be printed in the given timestamp, otherwise returns false.
-
 It is possible that several messages arrive roughly at the same time.
-
 Example:
-
 Logger logger = new Logger();
 
 // logging string "foo" at timestamp 1
@@ -24,28 +21,25 @@ Method below uses hashtable.
 Time: O(1)
 Space: O(n)
 '''
-class Logger(object):
+class Logger:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.a={}
+        self.dict = {}
 
-    def shouldPrintMessage(self, timestamp, message):
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         """
         Returns true if the message should be printed in the given timestamp, otherwise returns false.
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity.
-        :type timestamp: int
-        :type message: str
-        :rtype: bool
         """
-        if message not in self.a:
-            self.a[message] = timestamp
+        if message not in self.dict:
+            self.dict[message] = timestamp
             return True
-        elif timestamp-self.a[message]<10:
-            return False
+        elif self.dict[message]+10 <= timestamp:
+                self.dict[message] = timestamp
+                return True
         else:
-            self.a[message] = timestamp
-            return True
+            return False
