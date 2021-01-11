ce372ab2eb3a563521d6e158ebe9036d7a243180
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-11 10:37:28 -0500

    Create 146. LRU Cache.py

---
+++ 146. LRU Cache.py
@@ -0,0 +1,52 @@
+'''
+https://leetcode.com/problems/lru-cache/
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. 
+If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+
+Follow up:
+Could you do get and put in O(1) time complexity?
+Example 1:
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+'''
+'''
+Time:O(1)
+Space:O(n)
+'''
+from collections import OrderedDict
+class LRUCache(OrderedDict):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self:
+            return -1
+        self.move_to_end(key)
+        return self[key]
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self:
+            self.move_to_end(key)
+        self[key] = value
+        if len(self)>self.capacity:
+            self.popitem(last = False)
