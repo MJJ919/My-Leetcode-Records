096cdd477d1af4b719dbb66f6c6e1835e711adbc
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-11 10:47:29 -0500

    Update 303. Range Sum Query - Immutable.py

--- 303. Range Sum Query - Immutable.py
+++ 303. Range Sum Query - Immutable.py
@@ -1,35 +1,13 @@
 '''
+https://leetcode.com/problems/range-sum-query-immutable/
 Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
-
 Implement the NumArray class:
-
 NumArray(int[] nums) Initializes the object with the integer array nums.
 int sumRange(int i, int j) Return the sum of the elements of the nums array in the range [i, j] inclusive (i.e., sum(nums[i], nums[i + 1], ... , nums[j]))
 '''
-Method below is brute force, kinda awkward.
 '''
-
-class NumArray(object):
-
-    def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
-        self.data = nums
-
-    def sumRange(self, i, j):
-        """
-        :type i: int
-        :type j: int
-        :rtype: int
-        """
-        sum = 0
-        for a in range(i,j+1):
-            sum = sum + self.data[a]
-        return sum
-  
-'''
-Method below uses caching.
+Time:O(n)
+Space:O(n)
 '''
 
 class NumArray(object):
