51233f109a3cf1a1b67d802d8761fdab92274868
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-10 19:22:37 -0500

    Update 170. Two Sum III - Data structure design.py

--- 170. Two Sum III - Data structure design.py
+++ 170. Two Sum III - Data structure design.py
@@ -2,59 +2,13 @@
 Design a data structure that accepts a stream of integers and checks if it has a pair of integers that sum up to a particular value.
 
 Implement the TwoSum class:
-
 TwoSum() Initializes the TwoSum object, with an empty array initially.
 void add(int number) Adds number to the data structure.
 boolean find(int value) Returns true if there exists any pair of numbers whose sum is equal to value, otherwise, it returns false
 '''
-
 '''
-Method below uses 2 pointers.
-Remeber to sort the list first.
-'''
-
-class TwoSum(object):
-
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        global lst 
-        lst = []
-
-    def add(self, number):
-        """
-        Add the number to an internal data structure..
-        :type number: int
-        :rtype: None
-        """
-        lst.append(number)
-
-    def find(self, value):
-        """
-        Find if there exists any pair of numbers which sum is equal to the value.
-        :type value: int
-        :rtype: bool
-        """
-        i = 0
-        j = len(lst)-1
-        lst.sort()
-        while i < j:
-            if lst[i]+lst[j] == value:
-                return (i, j)
-            if lst[i]+lst[j] < value:
-                i = i+1
-            else:
-                j = j-1
-        return False
-
-# Your TwoSum object will be instantiated and called as such:
-# obj = TwoSum()
-# obj.add(number)
-# param_2 = obj.find(value)
-
-'''
-Method below uses hashtable.
+Time:O(1) O(n)
+Space:O(n)
 '''
 class TwoSum(object):
 
