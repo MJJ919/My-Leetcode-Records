09ac55a9367a2c60baa90e869e57e807a38ebd57
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-12 11:50:35 -0500

    Create 347. Top K Frequent Elements.py

---
+++ 347. Top K Frequent Elements.py
@@ -0,0 +1,20 @@
+'''
+https://leetcode.com/problems/top-k-frequent-elements/
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+'''
+'''
+Time:O(Nlogk)
+Space:O(N+k)
+'''
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key = count.get)
