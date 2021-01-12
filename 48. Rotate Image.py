11d0c0e3535b37ad3b74e78010a94333254287ea
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-12 17:15:45 -0500

    Update 48. Rotate Image.py

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -12,21 +12,18 @@ Example 2:
 Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 '''
-
 '''
 Method below makes the changes in place.
 Time:O(n**2)
 Space:O(1)
 '''
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix) 
         matrix.reverse()
-        for i in range(n):
-            for j in range(n):
-                matrix[i][j] = matrix[j][i]
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
         return matrix
