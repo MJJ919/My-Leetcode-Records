f37311b22d475b51cf294ad8c31494fc56a824d4
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-13 10:18:52 -0500

    Update 54. Spiral Matrix.py

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -1,5 +1,5 @@
 '''
-
+https://leetcode.com/problems/spiral-matrix/
 Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
 Example 1:
 Input:
@@ -19,37 +19,29 @@ Input:
 ]
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 '''
-
 '''
 Time:O(N)
 Space:O(1)
 '''
-class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
-        def spiral(r1,r2,c1,c2):
-            for c in range(c1,c2+1):
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        def spiral(r1, r2, c1, c2):
+            for c in range(c1, c2+1):
                 yield r1,c
-            for r in range(r1+1,r2+1):
-                yield r,c2
+            for r in range(r1+1, r2+1):
+                yield r, c2
             if c1<c2 and r1<r2:
-                for c in range(c2-1,c1,-1):
-                    yield r2,c
-                for r in range(r2,r1,-1):
+                for c in range(c2-1, c1,-1):
+                    yield r2, c
+                for r in range(r2, r1, -1):
                     yield r, c1
-        if not matrix:
-            return []
-        ans = []
-        r1, r2 = 0,len(matrix)-1
+                
+        r1, r2 = 0, len(matrix)-1
         c1, c2 = 0, len(matrix[0])-1
+        res = []
         while r1<=r2 and c1<=c2:
-            for r,c in spiral(r1,r2,c1,c2):
-                ans.append(matrix[r][c])
-            r1,r2=r1+1,r2-1
-            c1,c2=c1+1,c2-1
- 
-        return ans
-    
+            for r, c in spiral(r1, r2, c1, c2):
+                res.append(matrix[r][c])
+            r1, r2 = r1+1, r2-1
+            c1, c2 = c1+1, c2-1
+        return res
