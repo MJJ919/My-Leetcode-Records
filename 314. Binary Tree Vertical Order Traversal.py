6f622a1af58e6744c551371f0203fde8e61c290d
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-16 09:26:12 -0500

    Update 314. Binary Tree Vertical Order Traversal.py

--- 314. Binary Tree Vertical Order Traversal.py
+++ 314. Binary Tree Vertical Order Traversal.py
@@ -42,3 +42,27 @@ class Solution(object):
                 q.append((node.left, column-1))
                 q.append((node.right, column+1))
         return [d[x] for x in sorted(d.keys())]
+
+'''
+DFS
+'''
+ class Solution:
+    def verticalOrder(self, root: TreeNode) -> List[List[int]]:
+        def search(node, row, width):
+            if node:
+                nonlocal minwidth, maxwidth
+                d[width].append((row,node.val))
+                minwidth = min(minwidth, width)
+                maxwidth = max(maxwidth, width)
+                search(node.left, row+1, width-1)
+                search(node.right, row+1, width+1)
+                
+        if not root:    return []
+        d, res = defaultdict(list), []
+        minwidth = maxwidth = 0
+        search(root,0,0)
+        for i in range(minwidth, maxwidth+1):
+            d[i].sort(key=lambda x:x[0])
+            col = [val for row,val in d[i]]
+            res.append(col)
+        return res
