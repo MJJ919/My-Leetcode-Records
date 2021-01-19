3c72976628ecd0fa8f1cf8c99d75741031f2ed50
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-19 16:05:40 -0500

    Create 123. Best Time to Buy and Sell Stock III.py

---
+++ 123. Best Time to Buy and Sell Stock III.py
@@ -0,0 +1,35 @@
+'''
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+Say you have an array for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+'''
+'''
+Time:O(n)
+Space:O(n)
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices)<2:   return 0
+        minp1, maxp2 = float('inf'), float('-inf')
+        pro1, pro2 = 0, 0
+        n = len(prices)
+        p1, p2 = [0]*n, [0]*n
+        for i in range(n):
+            minp1 = min(minp1, prices[i])
+            pro1 = max(pro1, prices[i]-minp1)
+            p1[i] = pro1
+            j = n-i-1
+            maxp2 = max(maxp2, prices[j])
+            pro2 = max(pro2, maxp2-prices[j])
+            p2[j] = pro2
+        res = 0
+        for i in range(len(prices)):
+            res = max(res, p1[i]+p2[i])
+        return res
