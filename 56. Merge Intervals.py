850c1e7a9d4732ae7fa079f71cc250b8ed44500b
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-21 08:59:16 -0500

    Update 56. Merge Intervals.py

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -1,5 +1,5 @@
 '''
-
+https://leetcode.com/problems/merge-intervals/
 Given a collection of intervals, merge all overlapping intervals.
 
 Example 1:
@@ -7,27 +7,22 @@ Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
 Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 
-xample 2:
+Example 2:
 Input: intervals = [[1,4],[4,5]]
 Output: [[1,5]]
 Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 '''
-
 '''
-Time:O(NlogN)
+Time:O(nlgn)
 Space:O(n)
 '''
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key = lambda x:x[0])
-        merge=[]
-        for i in intervals:
-            if not merge or merge[-1][1]<i[0]:
-                merge.append(i)
+        res = [intervals[0]]
+        for i in range(1,len(intervals)):
+            if intervals[i][0]>res[-1][1]:
+                res.append(intervals[i])
             else:
-                merge[-1][1] = max(merge[-1][1],i[1])
-        return merge
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+        return res
