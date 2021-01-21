a0a2531d931659a70b4d355c953dec6cdb79ff0b
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-21 09:02:53 -0500

    Update 252. Meeting Rooms.py

--- 252. Meeting Rooms.py
+++ 252. Meeting Rooms.py
@@ -1,24 +1,24 @@
 '''
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+https://leetcode.com/problems/meeting-rooms/
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+determine if a person could attend all meetings.
 
 Example 1:
-
 Input: [[0,30],[5,10],[15,20]]
 Output: false
-Example 2:
 
+Example 2:
 Input: [[7,10],[2,4]]
 Output: true
 '''
-
-class Solution(object):
-    def canAttendMeetings(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: bool
-        """
-        intervals = sorted(intervals)
-        for i in range(len(intervals)-1):
-            if intervals[i+1][0] < intervals[i][1]:
+'''
+Time:O(nlgn)
+Space:O(1)
+'''
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key = lambda x:x[0])
+        for i in range(1,len(intervals)):
+            if intervals[i][0]<intervals[i-1][1]:
                 return False
         return True
