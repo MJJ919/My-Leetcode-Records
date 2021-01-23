c79aaf156c5e6eb240f63985f58ff0312b319e21
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-23 13:51:25 -0500

    Update 35. Search Insert Position.py

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -3,46 +3,19 @@ https://leetcode.com/problems/search-insert-position/
 Given a sorted array of distinct integers and a target value, return the index if the target is found. 
 If not, return the index where it would be if it were inserted in order.
 '''
-
 '''
 Time:O(logn)
 Space:O(1)
 '''
 class Solution:
-    def searchInsert(self, nums, target):
-        if target<nums[0]:
-            return 0
-        if target>nums[-1]:
-            return len(nums)
-        left, right = 0, len(nums)-1
-        while left<=right:
-            mid = (left+right)//2
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i, j = 0, len(nums)-1
+        while i<=j:
+            mid = (i+j)//2
             if target == nums[mid]:
                 return mid
-            elif target>nums[mid]:
-                left = mid+1
+            elif target > nums[mid]:
+                i += 1
             else:
-                right = mid-1
-        return left
-
-'''
-My method is a little bit awkward.
-'''
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        i = 0
-        if target < nums[0]:
-            return 0
-        if target > nums[-1]:
-            return len(nums)
-        else:
-            for i in range(len(nums)):
-                if target > nums[i]:
-                    i += 1
-                elif target <= nums[i]:
-                    return i
+                j -= 1
+        return i
