8377b63cf0560d27f83a21f7bbeb7f47308f7079
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-22 20:52:23 -0500

    Update 238. Product of Array Except Self.py

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -11,23 +11,20 @@ Constraint: It's guaranteed that the product of the elements of any prefix or su
 Time:O(n)
 Spaec:O(n)
 '''
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        l, r,product = [0]*n, [0]*n,[0]*n
-        l[0], r[n-1] = 1, 1
-        for i in range(1, n):
+        res = [0]*n
+        l = [1]*n
+        r = [1]*n
+        for i in range(1,n):
             l[i] = l[i-1]*nums[i-1]
         for i in reversed(range(n-1)):
             r[i] = r[i+1]*nums[i+1]
         for i in range(n):
-            product[i] = l[i]*r[i]
-        return product
- 
+            res[i] = r[i]*l[i]
+        return res
+    
 '''
 Time:O(n)
 Space:O(1)
