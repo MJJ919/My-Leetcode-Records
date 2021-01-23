95ded835e8e188fd9023c802bf48a0ea0ca1270b
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-22 22:54:19 -0500

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -10,25 +10,21 @@ You may assume that nums1 has enough space (size that is equal to m + n) to hold
 Time:O(n)
 Space:O(1)
 '''
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        i, j, k = m-1, n-1, m+n-1
-        while i>=0 and j>=0:
-            if nums1[i]>=nums2[j]:
-                nums1[k] = nums1[i]
-                i, k = i-1, k-1
+        i, j = m-1, n-1
+        for p in range(m+n-1, -1, -1):
+            if j<0:
+                break
+            if nums1[i]>nums2[j] and i>= 0:
+                nums1[p] = nums1[i]
+                i -= 1
             else:
-                nums1[k] = nums2[j]
-                j,k = j-1, k-1
-        nums1[:j+1] = nums2[:j+1]
-        return nums1
+                nums1[p] = nums2[j]
+                j -= 1
             
 '''
 Time:O(nlgn)
