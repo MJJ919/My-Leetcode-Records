c3f6f3690ede0c7cebaf708287ac2fab6c37ff78
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-26 15:45:26 -0500

    Update 383. Ransom Note.py

--- 383. Ransom Note.py
+++ 383. Ransom Note.py
@@ -9,10 +9,6 @@ Example 1:
 Input: ransomNote = "a", magazine = "b"
 Output: false
 
-Example 2:
-Input: ransomNote = "aa", magazine = "ab"
-Output: false
-
 Example 3:
 Input: ransomNote = "aa", magazine = "aab"
 Output: true
