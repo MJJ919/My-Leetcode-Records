06d3cbeb1e8d7569a296a1a3758a229e324db007
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-27 16:43:26 -0500

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,4 +1,5 @@
 '''
+https://leetcode.com/problems/valid-anagram/
 Given two strings s and t , write a function to determine if t is an anagram of s.
 
 Example 1:
@@ -9,25 +10,19 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 '''
-
 '''
 Time:O(n)
 Space:O(1)
 '''
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        d1, d2 = defaultdict(int), defaultdict(int)
-        for i in s:
-            d1[i] += 1
-        for j in t:
-            d2[j] += 1
-        return d1 == d2
-        
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):  return False
+        d1,d2 = defaultdict(int),defaultdict(int)
+        for i in range(len(s)):
+            d1[s[i]] += 1
+            d2[t[i]] += 1
+        return d1==d2
+    
 '''
 Time:O(nlgn)
 Space:O(1)
