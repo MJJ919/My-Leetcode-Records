966235225d5eb89bf19b57cd90cd877bde00283c
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-01-28 17:07:00 -0500

    Update 13.Roman to Integer.py

--- 13.Roman to Integer.py
+++ 13.Roman to Integer.py
@@ -25,19 +25,16 @@ Given a roman numeral, convert it to an integer.
 Time:O(1)   #Roman numeral has limited numbers.
 Space:O(1)
 '''
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def romanToInt(self, s: str) -> int:
         d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        res, i = 0, 0
-        while i<len(s)-1:
-            if d[s[i]] < d[s[i+1]]:
-                res -= d[s[i]]
+        res = 0
+        i = 0
+        while i<len(s):
+            if i < len(s)-1 and d[s[i]]<d[s[i+1]]:
+                res += d[s[i+1]]-d[s[i]]
+                i += 2
             else:
                 res += d[s[i]]
-            i += 1
-        res += d[s[-1]]
+                i += 1
         return res
