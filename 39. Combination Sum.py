90b2c4b9e7f73dcea0a58a331c5132247164dd1e
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-03 17:31:42 -0500

    Update 39. Combination Sum.py

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -15,23 +15,18 @@ It is guaranteed that the number of unique combinations that sum up to target is
 Time:
 Space:
 '''
-class Solution(object):
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        def back(target, start,cur):
-            if target==0:
-                res.append(cur[:])
-                return
-            elif target<0:
-                return
-            for i in range(start, length):
-                cur.append(candidates[i])
-                back(target-candidates[i], i, cur)
-                cur.pop()
-        res, length = [], len(candidates)
-        back(target, 0, [])
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def back(subset, start, target):
+            if target<0:
+                return 
+            elif target==0:
+                res.append(subset[:])
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                back(subset, i, target-nums[i])
+                subset.pop()
+        
+        res = []
+        back([], 0, target)
         return res
