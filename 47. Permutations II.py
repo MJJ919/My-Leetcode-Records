218718716500d306658de985eca1a60843839913
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-04 12:57:19 -0500

    Update 47. Permutations II.py

--- 47. Permutations II.py
+++ 47. Permutations II.py
@@ -7,22 +7,20 @@ Example 2:
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 '''
-'''
-backtracking.
-'''
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        def back(counter,cur):
-            if len(cur)==n:
-                res.append(cur[:])
-                return
-            for i in counter:
-                if counter[i]>0:
-                    cur.append(i)
-                    counter[i] -= 1
-                    back(counter, cur)
-                    cur.pop()
-                    counter[i] += 1
-        res, n = [], len(nums)
-        back(Counter(nums),[])
+        def back(subset):
+            if len(subset)==len(nums):
+                res.append(subset[:])
+            for num, freq in counter.items():
+                if freq>0:
+                    subset.append(num)
+                    counter[num] -= 1
+                    back(subset)
+                    subset.pop()
+                    counter[num] += 1
+        
+        counter = Counter(nums)
+        res = []
+        back([])
         return res
