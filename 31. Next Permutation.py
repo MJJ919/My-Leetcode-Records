1c3bc23bdd7bf5b57b74d898fd297ca998f50dc5
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-04 15:40:13 -0500

    Create 31. Next Permutation.py

---
+++ 31. Next Permutation.py
@@ -0,0 +1,36 @@
+'''
+https://leetcode.com/problems/next-permutation/
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+The replacement must be in place and use only constant extra memory.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: nums = [3,2,1]
+Output: [1,2,3]
+'''
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+        def reverse(n):
+            i, j = n, len(nums)-1
+            while i<j:
+                swap(i, j)
+                i,j = i+1, j-1
+            
+        i = len(nums)-2
+        while i>=0 and nums[i]>=nums[i+1]:
+            i -= 1
+        if i>=0:
+            j = len(nums)-1
+            while j>=i and nums[j]<=nums[i]:
+                j -= 1
+            swap(i,j)
+        reverse(i+1)
