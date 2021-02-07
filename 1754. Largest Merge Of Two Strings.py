b3bd375664455015a82fea1c169d6f03cf8cd01b
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-07 07:55:59 -0500

    Create 1754. Largest Merge Of Two Strings.py

---
+++ 1754. Largest Merge Of Two Strings.py
@@ -0,0 +1,20 @@
+'''
+https://leetcode.com/problems/largest-merge-of-two-strings/
+'''
+'''
+Time:O(n)
+Space:O(1)
+'''
+class Solution:
+    def largestMerge(self, word1: str, word2: str) -> str:
+        res = ''
+        while word1 and word2:
+            if word1>word2:
+                res += word1[0]
+                word1 = word1[1:]
+            else:
+                res += word2[0]
+                word2 = word2[1:]
+        res += word1
+        res += word2
+        return res
