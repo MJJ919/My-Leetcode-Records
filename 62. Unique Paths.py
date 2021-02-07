daf1a69580cf5bee4bc40509a7f3238b12723fff
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-07 10:13:22 -0500

    Update 62. Unique Paths.py

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -12,8 +12,9 @@ Space:O(M*N)
 '''
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        d = [[1]*n for i in range(m)]
-        for i in range(1,m):
-            for j in range(1,n):
-                d[i][j] = d[i-1][j]+d[i][j-1]
-        return d[-1][-1]
+        dp = [[1 for _ in range(n)]for _ in range(m)]
+        print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[-1][-1]
