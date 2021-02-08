6607963983848340008f9be5d286171e03f13c86
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-08 11:26:04 -0500

    Update 256. Paint House.py

--- 256. Paint House.py
+++ 256. Paint House.py
@@ -19,12 +19,14 @@ Space:O(1)
 '''
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        for n in reversed(range(len(costs)-1)):
-            costs[n][0] += min(costs[n+1][1], costs[n+1][2])
-            costs[n][1] += min(costs[n+1][0], costs[n+1][2])
-            costs[n][2] += min(costs[n+1][0], costs[n+1][1])
-        if len(costs)==0:   return 0
-        return min(costs[0])
+        if not costs:    return 0
+        dp = [[0 for _ in range(3)]for _ in range(len(costs))]
+        dp[0] = costs[0]
+        for i in range(1, len(costs)):
+            dp[i][0] = costs[i][0]+min(dp[i-1][1], dp[i-1][2])
+            dp[i][1] = costs[i][1]+min(dp[i-1][0], dp[i-1][2])
+            dp[i][2] = costs[i][2]+min(dp[i-1][1], dp[i-1][0])
+        return min(dp[-1])
 
 '''
 Memoization.
