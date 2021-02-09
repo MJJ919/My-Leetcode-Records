26f397cc68432cfb476f4818b78d050c8fc23ec7
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-09 08:48:18 -0500

    Update 198. House Robber.py

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -26,13 +26,13 @@ class Solution(object):
 Time:O(n)
 Space:O(n)
 '''
-class Solution(object):
-    def rob(self, nums):
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if not nums:    return 0
-        if len(nums)<3: return max(nums)
-        a = []
-        a.append(nums[0])
-        a.append(max(nums[0], nums[1]))
+        if len(nums)==1:    return nums[0]
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
         for i in range(2, len(nums)):
-            a.append(max(a[i-1], a[i-2]+nums[i]))
-        return a[-1]
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        return dp[-1]
