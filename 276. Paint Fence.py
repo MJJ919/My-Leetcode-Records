9aef5630d1589e92c716bc580e70fe8a64b0c8c7
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-09 11:16:24 -0500

    Update 276. Paint Fence.py

--- 276. Paint Fence.py
+++ 276. Paint Fence.py
@@ -24,3 +24,16 @@ class Solution:
         for i in range(n-2):
             pre, cur = cur, (cur+pre)*(k-1)
         return cur
+'''
+Time:O(n)
+Space:O(n)
+'''
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if n==0 or k==0:    return 0
+        if n==1:    return k
+        dp = [0 for _ in range(n)]
+        dp[0] = k; dp[1] = k*k
+        for i in range(2, n):
+            dp[i] = dp[i-1]*(k-1) + dp[i-2]*(k-1)
+        return dp[-1]
