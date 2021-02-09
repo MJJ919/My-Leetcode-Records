1969690ce668c5377a4d2e5d8d2285d9bf0eb87e
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-09 10:30:06 -0500

    Update 213. House Robber II.py

--- 213. House Robber II.py
+++ 213. House Robber II.py
@@ -27,3 +27,20 @@ class Solution:
         for i in range(0, len(nums)-1):
             pre2, now2 = now2, max(now2, pre2+nums[i])
         return max(now1, now2)
+'''
+Time:O(n)
+Space:O(n)
+'''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n==0:    return 0
+        if n<3: return max(nums)
+        dp1 = [0 for _ in range(n-1)]
+        dp2 = [0 for _ in range(n-1)]
+        dp1[0] = nums[0];dp1[1] = max(nums[0], nums[1])
+        dp2[0] = nums[1];dp2[1] = max(nums[1], nums[2])
+        for i in range(2, n-1):
+            dp1[i] = max(dp1[i-1], dp1[i-2]+nums[i])
+            dp2[i] = max(dp2[i-1], dp2[i-2]+nums[i+1])
+        return max(dp1[-1], dp2[-1])
