58bc4296c16034d073b0d353638b7d69741c942d
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-09 20:48:04 -0500

    Update 221. Maximal Square.py

--- 221. Maximal Square.py
+++ 221. Maximal Square.py
@@ -19,4 +19,23 @@ class Solution:
                 if matrix[i][j]=='1':
                     dp[i+1][j+1]=min(dp[i][j+1], dp[i+1][j], dp[i][j])+1
                     s = max(s, dp[i+1][j+1])
-        return s*shttps://leetcode.com/problems/maximal-square/
+        return s*s
+    
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0 for _ in range(n)]for _ in range(m)]
+        res = 0
+        for i in range(m):
+            dp[i][0] = int(matrix[i][0])
+            res = max(res, dp[i][0])
+        for j in range(1, n):
+            dp[0][j] = int(matrix[0][j])
+            res = max(res, dp[0][j])
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j]=='1':
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])+1  
+                    res = max(res, dp[i][j])
+        print(dp)
+        return res*res
