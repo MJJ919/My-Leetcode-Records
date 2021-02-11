a97085013206229288b4a28736e1afbbd59de531
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-11 16:00:06 -0500

    Update 35. Search Insert Position.py

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -12,10 +12,10 @@ class Solution:
         i, j = 0, len(nums)-1
         while i<=j:
             mid = (i+j)//2
-            if target == nums[mid]:
+            if nums[mid]==target:
                 return mid
-            elif target > nums[mid]:
-                i += 1
+            elif nums[mid]>target:
+                j = mid-1
             else:
-                j -= 1
-        return i
+                i = mid+1
+        return i 
