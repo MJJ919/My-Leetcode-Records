2e58715ad9473bb58770116fcbe8e9a61fe62b57
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-13 10:46:39 -0500

    Update 226. Invert Binary Tree.py

--- 226. Invert Binary Tree.py
+++ 226. Invert Binary Tree.py
@@ -8,10 +8,6 @@ Space:O(n)
 '''
 class Solution(object):
     def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
         if root:
             root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)        
         return root
