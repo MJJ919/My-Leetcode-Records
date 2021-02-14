9f209afd139c418a51673ca2a29db4c639ee3cd4
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-14 09:04:28 -0500

    Create 1760. Minimum Limit of Balls in a Bag.py

---
+++ 1760. Minimum Limit of Balls in a Bag.py
@@ -0,0 +1,33 @@
+'''
+https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/
+You are given an integer array nums where the ith bag contains nums[i] balls. You are also given an integer maxOperations.
+You can perform the following operation at most maxOperations times:
+Take any bag of balls and divide it into two new bags with a positive number of balls.
+For example, a bag of 5 balls can become two new bags of 1 and 4 balls, or two new bags of 2 and 3 balls.
+Your penalty is the maximum number of balls in a bag. You want to minimize your penalty after the operations.
+
+Return the minimum possible penalty after performing the operations.
+
+Example 1:
+Input: nums = [9], maxOperations = 2
+Output: 3
+Explanation: 
+- Divide the bag with 9 balls into two bags of sizes 6 and 3. [9] -> [6,3].
+- Divide the bag with 6 balls into two bags of sizes 3 and 3. [6,3] -> [3,3,3].
+The bag with the most number of balls has 3 balls, so your penalty is 3 and you should return 3.
+
+'''
+'''
+Time:O(nlgn)
+Space:O(1)
+'''
+class Solution:
+    def minimumSize(self, nums: List[int], m: int) -> int:
+        left, right = 1, max(nums)
+        while left<=right:
+            mid = (left+right)//2
+            if sum((i-1)//mid for i in nums)>m:
+                left = mid+1
+            else:
+                right = mid-1
+        return left
