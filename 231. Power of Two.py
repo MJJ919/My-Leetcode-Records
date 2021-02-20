a7ce4bd064255c8a845d79f6b22f317ae91bc662
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-19 22:03:04 -0500

    Create 231. Power of Two.py

---
+++ 231. Power of Two.py
@@ -0,0 +1,38 @@
+'''
+https://leetcode.com/problems/power-of-two/
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+'''
+'''
+Time:O(lgn)
+'''
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        while n%2 == 0:
+            n /= 2
+        return n==1
+    
+'''
+Time:O(1)
+'''
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n==0:
+            return False
+        return n&(-n)==n
+        
+'''
+Time:O(n)
+'''
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n==0:
+            return False
+        return n&(n-1)==0
