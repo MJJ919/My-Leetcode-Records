83ab8f6220b5a0d2be6da784f3973ae5b0d6b2dc
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-26 10:55:41 -0500

    Update 9.Palindrome Number.py

--- 9.Palindrome Number.py
+++ 9.Palindrome Number.py
@@ -42,3 +42,14 @@ class Solution(object):
                 reverse = x%10 + reverse*10
                 x = x//10
         return reverse == a
+    
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0 or (x%10==0 and x!=0):
+            return False
+        rev = 0
+        while rev<x:
+            last = x%10
+            x = x//10
+            rev = rev*10 + last
+        return rev == x or rev//10 == x
