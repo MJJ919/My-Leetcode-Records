159e7147a00d193fe85a934c682d8ad3c67435f0
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-02-27 08:46:33 -0500

    Update 12. Integer to Roman.py

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -30,12 +30,13 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), 
           (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
-        res = []
+        res = ''
         for digit, ch in digits:
-            if num==0:  break
+            if num<=0:
+                break
             count, num = divmod(num, digit)
-            res.append(ch*count)
-        return ''.join(res)
+            res += count*ch
+        return res
     
 class Solution:
     def intToRoman(self, num: int) -> str:
