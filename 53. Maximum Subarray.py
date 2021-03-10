23916f623cb4e453f23b0e7ac855f0fd492defe9
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-03-10 10:20:51 -0500

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -22,9 +22,7 @@ Space:O(1)
 '''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        s = 0
-        for i in range(len(nums)):
-            if s+nums[i]>nums[i]:
-                nums[i] = s+nums[i]
-            s = nums[i]
+        for i in range(1, len(nums)):
+            if nums[i-1]>0:
+                nums[i] += nums[i-1]
         return max(nums)
