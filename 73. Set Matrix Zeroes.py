d1765d5158f19eae35df59a2a6c7be8306996fef
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-03-12 22:59:12 -0500

    Update 73. Set Matrix Zeroes.py

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -22,3 +22,31 @@ class Solution:
             for j in range(c):
                 if i in row or j in col:
                     matrix[i][j] = 0
+'''
+Time:O(m*n)
+Space:O(1)
+'''
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])
+        iscol = False
+        for i in range(m):
+            if matrix[i][0]==0:
+                iscol = True
+            for j in range(1, n):
+                if matrix[i][j]==0:
+                    matrix[0][j]=0
+                    matrix[i][0]=0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0]==0 or matrix[0][j]==0:
+                    matrix[i][j]=0
+        if matrix[0][0]==0:
+            for j in range(n):
+                matrix[0][j]=0 
+        if iscol:
+            for i in range(m):
+                matrix[i][0]=0
