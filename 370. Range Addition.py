5f35793d467c1e4662e50ed03182126ffcee275a
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-03-17 08:42:23 -0400

    Update 370. Range Addition.py

--- 370. Range Addition.py
+++ 370. Range Addition.py
@@ -12,13 +12,13 @@ Space:O(1)
 '''
 class Solution:
     def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
-        res = [0]*(length+1)
-        for i in updates:
-            res[i[0]] += i[2]
-            res[i[1]+1] -= i[2]
-        temp = 0
-        for j in range(length):
-            temp += res[j]
-            res[j] = temp
+        res = [0 for _ in range(length+1)]
+        for update in updates:
+            start = update[0]
+            end = update[1]
+            res[start] += update[2]
+            res[end+1] -= update[2]
+        for i in range(1, length+1):
+            res[i] += res[i-1]
         res.pop()
         return res
