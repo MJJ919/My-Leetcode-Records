7c495a74e13e0ea27009f5e0b38b537cb7b1c443
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-04-05 16:14:44 -0400

    Update 168. Excel Sheet Column Title.py

--- 168. Excel Sheet Column Title.py
+++ 168. Excel Sheet Column Title.py
@@ -17,8 +17,9 @@ Space:O(1)
 '''
 class Solution:
     def convertToTitle(self, n: int) -> str:
-        res = []
-        while n>0:
-            res.append(chr((n-1)%26+65))
-            n = (n-1)//26
-        return ''.join(reversed(res))
+        res = ''
+        while n!=0:
+            n = n-1
+            res += chr(n%26 + 65)
+            n = n//26
+        return res[::-1]
