dc84a4c384e932eab1bbd341f61890ce3874ac37
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-04-17 16:23:06 -0400

    Create 1828. Queries on Number of Points Inside a Circle.py

---
+++ 1828. Queries on Number of Points Inside a Circle.py
@@ -0,0 +1,28 @@
+'''
+https://leetcode.com/problems/queries-on-number-of-points-inside-a-circle/
+You are given an array points where points[i] = [xi, yi] is the coordinates of the ith point on a 2D plane. 
+Multiple points can have the same coordinates.
+You are also given an array queries where queries[j] = [xj, yj, rj] describes a circle centered at (xj, yj) with a radius of rj.
+For each query queries[j], compute the number of points inside the jth circle. Points on the border of the circle are considered inside.
+Return an array answer, where answer[j] is the answer to the jth query.
+
+Example 1:
+Input: points = [[1,3],[3,3],[5,3],[2,2]], queries = [[2,3,1],[4,3,1],[1,1,2]]
+Output: [3,2,2]
+Explanation: The points and circles are shown above.
+queries[0] is the green circle, queries[1] is the red circle, and queries[2] is the blue circle.
+'''
+'''
+Time:O(n**2)
+Space:O(n)
+'''
+class Solution:
+    def countPoints(self, ps: List[List[int]], qs: List[List[int]]) -> List[int]:            
+        res = []            
+        for q in qs:
+            count = 0
+            for p in ps:
+                if (q[0]-p[0])**2 + (q[1]-p[1])**2 <= q[2]**2:
+                    count += 1
+            res.append(count)
+        return res
