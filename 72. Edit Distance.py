16f29dc643e672e19c106e9ea75b7100783b8d24
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-05-08 14:57:00 -0400

    Update 72. Edit Distance.py

--- 72. Edit Distance.py
+++ 72. Edit Distance.py
@@ -22,17 +22,18 @@ Time:O(m*n)
 Space:O(m*n)
 '''
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
-        dp = [[0 for _ in range(n+1)]for i in range(m+1)]
-        for i in range(1, m+1):
+    def minDistance(self, w1: str, w2: str) -> int:
+        m = len(w1)
+        n = len(w2)
+        dp = [[0 for _ in range(n+1)]for _ in range(m+1)]
+        for i in range(m+1):
             dp[i][0] = i
-        for j in range(1, n+1):
+        for j in range(n+1):
             dp[0][j] = j
         for i in range(1, m+1):
             for j in range(1, n+1):
-                if word1[i-1]==word2[j-1]:
-                    dp[i][j] = 1+min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]-1)
+                if w1[i-1]==w2[j-1]:
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]+1, dp[i][j-1]+1)
                 else:
-                    dp[i][j] = 1+min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                    dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
         return dp[-1][-1]
