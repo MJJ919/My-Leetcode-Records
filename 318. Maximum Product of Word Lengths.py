9b56669ae97a347d83f09409abd8b4b3a738935c
MJJ919 <63932852+MJJ919@users.noreply.github.com>
2021-05-27 10:58:34 -0400

    Update 318. Maximum Product of Word Lengths.py

--- 318. Maximum Product of Word Lengths.py
+++ 318. Maximum Product of Word Lengths.py
@@ -9,6 +9,29 @@ Output: 16
 Explanation: The two words can be "abcw", "xtfn".
 '''
 '''
+Time:O(n**2+L)
+Space:O(n)
+'''
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        n = len(words)
+        mask = [0 for _ in range(n)]
+        bit = lambda x:ord(x)-ord('a')
+        
+        for i in range(n):
+            bitmask = 0
+            for ch in words[i]:
+                bitmask |= 1<<bit(ch)
+            mask[i] = bitmask
+            
+        res = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                if mask[i] & mask[j]==0:
+                    res = max(res, len(words[i])*len(words[j]))
+    
+        return res
+'''
 Time:
 Space:O(1)
 '''
