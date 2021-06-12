2745f96f426dabcd797d19acd667b23dca05b5af
MJJ <63932852+MJJ919@users.noreply.github.com>
2021-06-12 10:23:04 -0400

    Update 57. Insert Interval.py

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -19,20 +19,33 @@ Space:O(n)
 class Solution:
     def insert(self, intervals: List[List[int]], new: List[int]) -> List[List[int]]:
         res = []
-        start, end = new[0], new[1]
-        i = 0
-        while i<len(intervals) and intervals[i][0]<start:
-                res.append(intervals[i])
-                i += 1
-        if not res or res[-1][1]<start:
+        for inter in intervals:
+            if not new or inter[1]<new[0]:
+                res.append(inter)
+            elif inter[0]>new[1]:
+                res.append(new)
+                res.append(inter)
+                new = None
+            else:
+                new[0] = min(new[0], inter[0])
+                new[1] = max(new[1], inter[1])
+        if new:
             res.append(new)
-        else:
-            res[-1][1] = max(res[-1][1], end)
-            
-        while i<len(intervals):
-            if res[-1][1]<intervals[i][0]:
+        return res
+    
+'''
+Time:O(nlgn)
+Space:O(n)
+'''
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort()
+        res = []
+        res.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if intervals[i][0]>res[-1][1]:
                 res.append(intervals[i])
             else:
                 res[-1][1] = max(res[-1][1], intervals[i][1])
-            i += 1
         return res
