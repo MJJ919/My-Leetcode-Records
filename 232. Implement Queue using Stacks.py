22009f0a433f79efd91ca8aef263b7e9a5677198
MJJ <63932852+MJJ919@users.noreply.github.com>
2021-06-28 10:02:45 -0400

    Update 232. Implement Queue using Stacks.py

--- 232. Implement Queue using Stacks.py
+++ 232. Implement Queue using Stacks.py
@@ -18,18 +18,10 @@ Space:O(n)
 class MyQueue(object):
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.d1 = []
         self.d2 = []
 
     def push(self, x):
-        """
-        Push element x to the back of queue.
-        :type x: int
-        :rtype: None
-        """
         while self.d1:
             self.d2.append(self.d1.pop())
         self.d1.append(x)
@@ -37,24 +29,12 @@ class MyQueue(object):
             self.d1.append(self.d2.pop())
 
     def pop(self):
-        """
-        Removes the element from in front of queue and returns that element.
-        :rtype: int
-        """
-        return self.d1.pop()
+       return self.d1.pop()
 
     def peek(self):
-        """
-        Get the front element.
-        :rtype: int
-        """
         return self.d1[-1]
 
     def empty(self):
-        """
-        Returns whether the queue is empty.
-        :rtype: bool
-        """
         return not self.d1
         
 '''
