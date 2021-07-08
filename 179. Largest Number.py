3a473778a0650a4b515ee6da79509ec661bf5132
MJJ <63932852+MJJ919@users.noreply.github.com>
2021-07-08 06:43:08 -0400

    Update 179. Largest Number.py

--- 179. Largest Number.py
+++ 179. Largest Number.py
@@ -46,12 +46,14 @@ class Solution {
 Time:O(nlgn)
 Space:O(n)
 '''
-class Solution(object):
-    def largestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: str
-        """
-        nums = map(str, nums)
-        nums.sort(cmp = lambda x, y: cmp(x + y, y + x), reverse = True)
-        return '0' if nums[0] == '0' else ''.join(nums)
+import functools
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def comparator(n1, n2):
+            if int(n1+n2)>int(n2+n1):
+                return -1
+            else:
+                return 1
+        nums = [str(x) for x in nums]
+        nums.sort(key = functools.cmp_to_key(comparator))
+        return ''.join(nums) if nums[0]!= '0' else '0'
